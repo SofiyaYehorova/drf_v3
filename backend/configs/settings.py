e4c50879302779dac439e4484cbbc09414b387cb
SofiyaYehorova <yehorova66@gmail.com>
2023-09-11 14:44:40 +0300

    lesson_9_celery_redis

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
+    'celery',
+    'django_celery_results',
+    'django_celery_beat',
 
     # my apps
     'core',
--- backend/core/services/email_service.py
+++ backend/core/services/email_service.py
@@ -1,14 +1,21 @@
 import os
 
-from django.template.loader import get_template
+from django.contrib.auth import get_user_model
 from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+
+from configs.celery import app
 
 from core.dataclasses.user_dataclass import UserDataClass
-from core.services.jwt_service import JWTService, ActivateToken, RecoveryToken
+from core.services.jwt_service import ActivateToken, JWTService, RecoveryToken
 
+from apps.users.models import UserModel as User
+
+UserModel: User = get_user_model()
 
 class EmailService:
     @staticmethod
+    @app.task
     def __send_email(to: str, template_name: str, context: dict, subject: ''):
         template = get_template(template_name)
         html_content = template.render(context)
@@ -17,17 +24,24 @@ class EmailService:
         msg.send()
 
     @classmethod
-    def test_email(cls):
-        cls.__send_email('yehorova66@gmail.com', 'test_email.html', {}, 'Hello')
+    def test_email(cls, user: UserDataClass):
+        # cls.__send_email('yehorova66@gmail.com', 'test_email.html', {}, 'Hello')
+        cls.__send_email(user.email, 'test_email.html', {}, 'Hello')
 
     @classmethod
     def register_email(cls, user: UserDataClass):
         token = JWTService.create_token(user, ActivateToken)
         url = f'http://localhost:3000/activate/{token}'
-        cls.__send_email(user.email, 'register.html', {'name': user.profile.name, 'url': url}, 'Register')
+        cls.__send_email.delay(user.email, 'register.html', {'name': user.profile.name, 'url': url}, 'Register')
 
     @classmethod
     def recovery_password(cls, user:UserDataClass):
         token = JWTService.create_token(user, RecoveryToken)
         url = f'http://localhost:3000/recovery/{token}'
         cls.__send_email(user.email, 'recovery_password.html', {'name':user.profile.name, 'url':url}, 'Recovery password')
+
+    @staticmethod
+    @app.task
+    def spam():
+        for user in UserModel.objects.all():
+            EmailService.__send_email(user.email, 'spam.html', {}, 'SPAM')
